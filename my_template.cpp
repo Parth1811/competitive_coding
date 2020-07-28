916792c0f840b02609688fc0715004e353ffd2df
parth <parthvin@gmail.com>
2020-07-28 21:58:20 +0530

    added my template

---
+++ my_template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+
+  return 0;
+}
